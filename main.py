a1ae174d6c2a6bcda7954a6df318b90534741b12
Pedro Henrique R. da Mata <pedrohenrique.phr38@gmail.com>
2020-07-25 05:43:56 +0000

    main v1.0

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
-from tkinter import *
 import names
-def tfb(app,texto,x,y,w,h):
-  Label(app,text=texto,background="#dde", foreground="#009",anchor=W).place(x=x,y=y,width=w,h=20)
-def button(app,texto,comando, x,y,w,h):
-  btn = Button(app, text=texto,command=comando).place(x=x,y=y,width=w,h=20)
-def dados():
-  global menu 
-  menu = int(form.get())
-  print(menu)
-app=Tk()
-app.configure(background="#dde")
-app.geometry("1280x720")
+from colorama import Fore, init, Style
+import os
+init(autoreset=True)
 escolha = 2
-counter = 0
-nomes = []
+counter = 0 ; terminador = 1 
 counter2= 11
 list_counter = 1
-menu = "===== Gerador de nomes =====\nMenu:\n1 --> Nomes masculinos\n2 --> Nomes femininos"
-tfb(app,"===== Gerador de nomes =====",10,10,250,20)
-tfb(app,"Menu:",10,30,250,20)
-tfb(app,"1 --> Nomes masculinos",10,50,250,20)
-tfb(app,"2 --> Nomes femininos",10,70,250,20)
-tfb(app,"Insira sua escolha",10,90,250,20)
-menu = 0
-form = Entry(app)
-form.place(x=10,y=110, width=200,height=20)
-button(app,"Enviar",dados,10,140,75,75)
-print(menu)
-#print("===== Gerador de nomes =====\nMenu:\n1 --> Nomes masculinos\n2 --> Nomes femininos")
-
+print(Fore.CYAN + "===== Gerador de nomes ====="+ Fore.RESET + "\nMenu:" + Fore.LIGHTMAGENTA_EX + "\n1 --> Nomes masculinos" + Fore.BLUE + "\n2 --> Nomes femininos")
+menu = int(input(Fore.CYAN + Style.BRIGHT + "Insira sua escolha: "))
 if menu == 1: 
   genero = "male"
 elif menu == 2:
   genero = "female"
 else: 
-    print("Opção inválida.")
-try:
-  #tfb(app,"Insira sua escolha",10,130,250,20)
-  print("===== Lista {} =====".format(list_counter))
-  while(escolha != 1 and counter < counter2):
-      
-      nome=names.get_full_name(gender=genero)
-      counter+=1
-      print("{}. {}".format(counter+1,nome))
-      nomes.append(nome)
-      if counter == (counter2-1):
-          escolha = int(input(""))
+  print(Fore.RED + "Opção inválida.")
+  terminador = 0
+os.system('clear')
+if terminador != 0:
+  print(Style.RESET_ALL + Fore.YELLOW + "\nGostaria de escolher a inicial?"+ Fore.WHITE +"\n1 --> Sim\n2 --> Não")
+  menu = int(input(Fore.YELLOW + "Insira sua escolha: "))
+  if menu == 1: 
+    inicial = str(input("Insira a inicial: ")).title()
+    terminador=1
+  elif menu == 2:
+    print(Fore.LIGHTYELLOW_EX + "Nomes completamente aleatórios serão gerados!\n")
+    terminador=1
+    inicial = "-"
+  else: 
+    terminador = 0
+    print(Fore.RED + "Opção inválida.")
+os.system('clear')
+if terminador == 1:
+  try:
+    print(Fore.GREEN + "\n===== Lista {} =====".format(list_counter))
+    while(escolha != 1 and counter < counter2):
+        if inicial != "-":
+          nome=names.get_full_name(gender=genero)
+          #print(nome[0:0])
+          if nome[0:1] == inicial:
+            counter+=1
+            print(Fore.YELLOW + str(counter) + ". " + Fore.LIGHTYELLOW_EX + str(nome))
+        else:
+          nome=names.get_full_name(gender=genero)
+          counter+=1
+          print(Fore.YELLOW + str(counter) + ". " + Fore.LIGHTYELLOW_EX + str(nome))
+        if counter == (counter2-1):
+          escolha = int(input(Fore.GREEN + "Digite: "))
+          list_counter+=1
           counter2= counter2 + 10 
-except:
-  print("O programa não pode continuar.")
-print("Fim do programa.")        
-
-
-app.mainloop()
+          os.system('clear')
+          if escolha != 1:
+            print(Fore.GREEN + "\n===== Lista {} =====".format(list_counter))
+  except:
+    print(Fore.RED + "O programa não pode continuar.")
+print(Fore.RED + "Fim do programa")    
